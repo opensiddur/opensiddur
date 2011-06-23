5d046ba6775ac5faf58b6ff22a9d5f446ebade1e
Efraim Feinstein <efraim.feinstein@gmail.com>
2011-06-22 22:13:51 -0400

    More testing/documentation for user profile

--- code/tests/api/user/user.py
+++ code/tests/api/user/user.py
@@ -174,81 +174,89 @@ class Test_Change_Password_For_User_That_Is_Logged_In(Test_Create_User, unittest
 ######## User profile editing ##########
 
 class User_Profile_Operations(Test_Create_User):
+  profileOperation = '' # superclasses *must* set the profileOperation; newUserName is used for the user name
+  profileContent = ''   # superclasses *must* set profileContent
+  profileReturn = ''    # expected return value of get if set to profileContent
+  profileFormat = ''   # superclasses *must* set profileFormat. If it's left empty, no format is used
+
   def setUp(self):
     super(Test_Create_User, self).setUp()
+    self.userName = self.newUserName
     self.create_user(self.newUserName, self.newPassword)
     self.assertStatusError(self.code, 201)
 
   # each user's profile is represented by a number of individual API calls
   # they all accept both txt and xml formats
 
-  def contentType(self, format):
-    if format == 'txt':
+  def contentType(self):
+    if self.profileFormat == 'txt':
       return 'text/plain'
     else:
       return 'application/xml'
 
-  def get_user_profile_menu(self, name):
-    (self.code, self.reason, self.data) = self.database.get('/code/api/user/' + name)
+  def extension(self):
+    return ('.' + self.profileFormat) if self.profileFormat else ''
+      
 
-  def get_user_profile_content(self, name, profile, format):
-    (self.code, self.reason, self.data) = self.database.get('/code/api/user/' + name + '/' + profile + '.' + format) 
+  def get_menu(self):
+    (self.code, self.reason, self.data) = self.database.get('/code/api/user/' + self.profileOperation)
 
-  def set_user_profile_content(self, name, profile, format, content):
-    (self.code, self.reason, self.data) = self.database.put('/code/api/user/' + name + '/' + profile + '.' + format, content, self.contentType(format)) 
+  def get_content(self):
+    (self.code, self.reason, self.data) = self.database.get('/code/api/user/' + self.userName + '/' + self.profileOperation +  self.extension()) 
+
+  def set_content(self):
+    (self.code, self.reason, self.data) = self.database.put('/code/api/user/' + self.userName + '/' + self.profileOperation + self.extension(), 
+      self.profileContent, self.contentType()) 
   
-  def clear_user_profile_content(self, name, profile, format):
-    (self.code, self.reason, self.data) = self.database.delete('/code/api/user/' + name + '/' + profile) 
+  def clear_content(self, name, profile, format):
+    (self.code, self.reason, self.data) = self.database.delete('/code/api/user/' + self.userName + '/' + self.profileOperation) 
 
 class Test_Set_Name_Txt_Format(User_Profile_Operations, unittest.TestCase):
-  newName = 'Rabbi Test von User III'
-
-  def set_name(self, content):
-    self.set_user_profile_content(self.newUserName, 'name', 'txt', content)
+  profileContent = 'Rabbi Test von User III'   # this is the name we will use for testing
+  profileOperation = 'name'
+  profileFormat = 'txt'
+  profileReturn = profileContent
 
   def test_Set_returns_HTTP_status_204(self):
-    self.set_name(self.newName)
+    self.set_content()
     self.assertResponse(self.code, 204, self.reason, self.data)
 
 class Test_Get_Name_Txt_Format(Test_Set_Name_Txt_Format):
   def setUp(self):
     super(Test_Get_Name_Txt_Format, self).setUp()
-    self.set_name(self.newName)
+    self.set_content()
     self.assertStatusError(self.code, 204, self.data)
 
-  def get_name(self):
-    self.get_user_profile_content(self.newUserName, 'name', 'txt')
-  
   def test_Get_returns_HTTP_status_200(self):
-    self.get_name()
+    self.get_content()
     self.assertResponse(self.code, 200, self.reason, self.data)
 
-  def test_Get_returns_the_same_name_that_was_set(self):
-    self.get_name()
-    self.assertTrue(self.data == self.newName)
+  def test_Get_returns_the_same_content_that_was_set(self):
+    self.get_content()
+    self.assertTrue(self.data == self.profileReturn)
 
 class Test_Set_Name_Xml_Format(Test_Set_Name_Txt_Format, unittest.TestCase):
   # note: set name in XML format breaks the usual convention that put->get return the same
   # thing. it will have the same string value, but the db will process the name into
   # component parts.
-  def set_name(self, content):
-    self.set_user_profile_content(
-      self.newUserName, 'name', 'xml',
-        '<tei:name xmlns:tei="http://www.tei-c.org/ns/1.0">' + content + '</tei:name>')
+  profileReturn = Test_Set_Name_Txt_Format.profileContent
+  profileContent = '<tei:name xmlns:tei="http://www.tei-c.org/ns/1.0">' + profileReturn + '</tei:name>'
+  profileFormat = 'xml'
 
 
 class Test_Get_Name_Xml_Format(Test_Get_Name_Txt_Format, unittest.TestCase):
-  def get_name(self):
-    self.get_user_profile_content(self.newUserName, 'name', 'xml')
+  profileReturn = Test_Set_Name_Txt_Format.profileContent
+  profileContent = '<tei:name xmlns:tei="http://www.tei-c.org/ns/1.0">' + profileReturn + '</tei:name>'
+  profileFormat = 'xml'
 
-  def test_Get_returns_the_same_name_that_was_set(self):
-    self.get_name()
+  def test_Get_returns_the_same_content_that_was_set(self):
+    self.get_content()
     self.assertTrue(
-      self.toTree(self.data).xpath("normalize-space(/tei:name)='%s'" % self.newName, namespaces=self.prefixes)
+      self.toTree(self.data).xpath("normalize-space(/tei:name)='%s'" % self.profileReturn, namespaces=self.prefixes)
     )
 
   def test_Get_returns_the_name_broken_up_into_XML_components(self):
-    self.get_name()
+    self.get_content()
     self.assertTrue(
       self.toTree(self.data).xpath(
         "count(/tei:name[tei:roleName='Rabbi' and" +
@@ -257,6 +265,91 @@ class Test_Get_Name_Xml_Format(Test_Get_Name_Txt_Format, unittest.TestCase):
         "tei:surname='User' and" +
         "tei:genName='III'])=1", namespaces=self.prefixes)
     )
+
+#### User profile properties: email ####
+
+class Test_Set_Email_Txt_Format(Test_Set_Name_Txt_Format, unittest.TestCase):
+  # setting an email and setting a name has the same tests...
+  profileContent = 'test@test.example.com'
+  profileOperation = 'email'
+  profileFormat = 'txt'
+  profileReturn = profileContent
+
+class Test_Get_Email_Txt_Format(Test_Get_Name_Txt_Format, Test_Set_Email_Txt_Format):
+  # get email has the same tests and setup as get name and the same parameters as set email
+  pass
+
+class Test_Set_Email_Xml_Format(Test_Set_Name_Txt_Format, unittest.TestCase):
+  # set email in xml format
+  profileReturn = Test_Set_Email_Txt_Format.profileContent
+  profileOperation = 'email'
+  profileContent = '<tei:email xmlns:tei="http://www.tei-c.org/ns/1.0">' + profileReturn + '</tei:email>'
+  profileFormat = 'xml'
+
+class Test_Get_Email_Xml_Format(Test_Get_Email_Txt_Format, unittest.TestCase):
+  profileContent = '<tei:email xmlns:tei="http://www.tei-c.org/ns/1.0">' + Test_Set_Email_Txt_Format.profileContent + '</tei:email>'
+  profileReturn = profileContent
+  profileFormat = 'xml'
+
+  def test_Get_returns_the_same_content_that_was_set(self):
+    self.get_content()
+    self.assertTrue(
+      self.treesEqual(
+        self.toTree(self.data), self.toTree(self.profileReturn)
+      )
+    )
+
+#### User profile properties: organization name ####
+class Test_Set_Organization_Name_Txt_Format(Test_Set_Name_Txt_Format, unittest.TestCase):
+  # setting an orgname and setting a name have the same tests...
+  profileContent = 'My Organization'
+  profileOperation = 'orgname'
+  profileFormat = 'txt'
+  profileReturn = profileContent
+
+class Test_Get_Organization_Name_Txt_Format(Test_Get_Name_Txt_Format, Test_Set_Organization_Name_Txt_Format):
+  # get orgname has the same tests and setup as get name and the same parameters as set organization name
+  pass
+
+class Test_Set_Organization_Name_Xml_Format(Test_Set_Name_Txt_Format, unittest.TestCase):
+  # set orgname in xml format
+  profileReturn = Test_Set_Organization_Name_Txt_Format.profileContent
+  profileOperation = 'orgname'
+  profileContent = '<tei:orgName xmlns:tei="http://www.tei-c.org/ns/1.0">' + profileReturn + '</tei:orgName>'
+  profileFormat = 'xml'
+
+class Test_Get_Organization_Name_Xml_Format(Test_Get_Email_Xml_Format, unittest.TestCase):
+  # tests for get organization name are the same as get email
+  profileOperation = 'orgname'
+  profileContent = '<tei:orgName xmlns:tei="http://www.tei-c.org/ns/1.0">' + Test_Set_Organization_Name_Txt_Format.profileContent + '</tei:orgName>'
+  profileReturn = profileContent
+  profileFormat = 'xml'
+
+#### User profile properties: foobar (a property that does not exist) ####
+class Test_Set_Nonexistent_Txt_Format(User_Profile_Operations, unittest.TestCase):
+  # setting an orgname and setting a name have the same tests...
+  profileContent = 'WhoCares'
+  profileOperation = 'foobar'
+  profileFormat = 'txt'
+  profileReturn = profileContent
+
+  def test_Set_returns_HTTP_status_404(self):
+    self.set_content()
+    self.assertResponse(self.code, 404, self.reason, self.data)
+
+class Test_Get_Nonexistent_Txt_Format(Test_Set_Nonexistent_Txt_Format):
+  # get orgname has the same tests and setup as get name and the same parameters as set organization name
+  def test_Get_returns_HTTP_status_404(self):
+    self.get_content()
+    self.assertResponse(self.code, 404, self.reason, self.data)
+
+class Test_Set_Nonexistent_Xml_Format(Test_Set_Nonexistent_Txt_Format, unittest.TestCase):
+  # same as for txt format
+  profileFormat = 'xml'
+
+class Test_Get_Nonexistent_Xml_Format(Test_Get_Nonexistent_Txt_Format, unittest.TestCase):
+  # tests for get nonexistent in xml are the same as for txt
+  profileFormat = 'xml'
 
 ######## End tests ###########
 def usage():
