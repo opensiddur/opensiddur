8c14225c27b86054b0158af24a6ddb204246e48c
Efraim Feinstein <efraim.feinstein@gmail.com>
2019-02-10 09:20:38 -0800

    debugging: do not remove files, verbose removal

--- python/removeFromBackup.py
+++ python/removeFromBackup.py
@@ -15,6 +15,7 @@ from lxml import etree
 
 exNS = 'http://exist.sourceforge.net/NS/exist'
 
+verbose = False
 
 def load_regexps(f):
     return re.compile("|".join([line.strip() for line in f if line.strip()]))
@@ -35,6 +36,8 @@ def should_remove(pth, remove_regexps, elem, remove_system_files=True):
 def remove_from_filesystem(directory, filename):
     full_path = os.path.join(directory, filename)
     try:
+        if verbose:
+            print("Removing ", full_path)
         os.remove(full_path)
     except OSError:  # it's a directory
         shutil.rmtree(full_path)
@@ -61,8 +64,10 @@ def remove_from_backup(path_to_content_xml, remove_regexps, remove_system_files)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--include_system_files', action="store_false", dest="remove_system_files", default=True)
+    parser.add_argument('-v', '--verbose', action="store_true", dest="verbose", default=False)
     parser.add_argument("path", action="store")
     args = parser.parse_args()
 
+    verbose = args.verbose
     toRemove = load_regexps(sys.stdin)
     remove_from_backup(args.path, toRemove, args.remove_system_files)
