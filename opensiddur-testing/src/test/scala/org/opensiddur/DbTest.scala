92248be43b7c5ff1dd5a5e93f5a65561b60a7be5
Efraim Feinstein <efraim.feinstein@gmail.com>
2020-09-28 23:44:48 -0700

    DbTest class and data module test start

--- opensiddur-testing/src/test/scala/org/opensiddur/DbTest.scala
+++ opensiddur-testing/src/test/scala/org/opensiddur/DbTest.scala
@@ -1,22 +1,98 @@
 package org.opensiddur
 
-import org.scalatest.BeforeAndAfter
+import org.exist.xmldb.EXistResource
+import org.opensiddur.DbTest._
 import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.{BeforeAndAfterEach, BeforeAndAfterAll}
+import org.xmldb.api.DatabaseManager
+import org.xmldb.api.base._
+import org.xmldb.api.modules._
 
-class DbTest extends AnyFunSpec with BeforeAndAfter {
-  before {
-    print("Before")
+case class XQueryTest(
+                     expr: String,
+                     failureMessage: String,
+                     result: Boolean = true
+                     )
+
+abstract class DbTest extends AnyFunSpec with BeforeAndAfterEach with BeforeAndAfterAll {
+  def initDb(): Unit = {
+    val cl = Class.forName(driver)
+    val database = cl.newInstance.asInstanceOf[Database]
+    //database.setProperty("create-database", "true")
+    DatabaseManager.registerDatabase(database)
   }
 
-  after {
-    print("After")
+  /** imports, namespaces and variables */
+  def prolog: String
+
+  /** run a generic XQuery. return the content as strings */
+  protected def runXQuery(query: String): Array[String] = {
+    var results = Array[String]()
+    val col = DatabaseManager.getCollection(existUri + "/db")
+    try {
+      val xqs = col.getService("XQueryService", "1.0").asInstanceOf[XQueryService]
+      val compiled = xqs.compile(prolog + "\n" + query)
+      val result = xqs.execute(compiled)
+      val iter = result.getIterator
+      while (iter.hasMoreResources) {
+        var res: EXistResource = null
+        try {
+          res = iter.nextResource.asInstanceOf[EXistResource]
+          results = results :+ res.getContent.toString
+        } finally {
+          // cleanup resources
+          res.freeResources()
+        }
+      }
+    }
+    finally {
+      if (col != null) {
+        col.close()
+      }
+    }
+    results
   }
 
-  it("should do something") {
-    assert(true)
+  /** run xquery setup code. The XQuery result is ignored/assumed to be empty */
+  def xquerySetup(query: String): Unit = {
+    runXQuery(query)
   }
 
-  it("should fail") {
-    assert(false)
+  /** run xquery teardown code. The XQuery result is ignored/assumed to be empty */
+  def xqueryTearDown(query: String): Unit = {
+    runXQuery(query)
   }
+
+  /** Run an XQuery test or set of tests.
+   *
+   *  @param codeUnderTest XQuery code to test. Will be placed in a let expression called $output
+   *  @param testExpressions XQuery test code and failure messages. Each test must resolve to a boolean. It may reference $output
+   */
+  def xqueryTest(
+                  codeUnderTest: String,
+                  testExpressions: Seq[XQueryTest]
+                ): Unit = {
+    val testingQuery = prolog + "\n" +
+      "let $output := \n" +
+        s"$codeUnderTest\n" +
+      "return (\n" +
+      "<output>{$output}</output>\n" +
+      testExpressions.map { testExpression =>
+        s"if (${testExpression.expr}) then 0 else 1"
+      }.mkString(",\n") + "\n)"
+    val results = runXQuery(testingQuery)
+    val output = results.head
+    testExpressions.zip(results.tail).foreach { case (testExpression, result) =>
+      assert(result == "1",
+      s"${testExpression.failureMessage}\nfrom: ${testExpression.expr}\non $output")
+    }
+  }
+}
+
+object DbTest {
+  // docker port for eXistdb
+  val existPort = 5000
+
+  val driver = "org.exist.xmldb.DatabaseImpl"
+  val existUri = s"xmldb:exist://localhost:${existPort}/exist/xmlrpc"
 }
---
+++ opensiddur-testing/src/test/scala/org/opensiddur/modules/TestData.scala
@@ -0,0 +1,10 @@
+package org.opensiddur.modules
+
+import org.opensiddur.DbTest
+
+class TestData extends DbTest {
+  def prolog =
+    """xquery version 3.1;
+      |
+      |""".stripMargin
+}
