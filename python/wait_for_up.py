fc3fa22cab9f7d2f29db49f470d570aeeecb1ad7
Efraim Feinstein <efraim.feinstein@gmail.com>
2020-11-11 23:15:22 -0800

    use an older format

--- python/wait_for_up.py
+++ python/wait_for_up.py
@@ -9,7 +9,7 @@ ap.add_argument("--port", default=5000, dest="port", type=int)
 ap.add_argument("--timeout", default=86400, dest="timeout", type=int)
 args = ap.parse_args()
 
-print(f"Waiting {args.timeout}s for server {args.host}:{args.port} to be up...")
+print("Waiting {}s for server {}:{} to be up...".format(args.timeout, args.host, args.port))
 if not testrunner.wait_for_uptime(host=args.host, port=args.port, max_timeout_s=args.timeout):
     print("Server is down.")
     sys.exit(2)
